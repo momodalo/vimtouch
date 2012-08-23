6910fbba9f4507c00f55384a737e63eb0284e066
David Tao <momodalo@gmail.com>
2012-08-23 13:09:51 +0800

    import EmulatorView from Android Terminal Emulator

---
+++ libraries/emulatorview/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/momodalo/projects/vimtouch/vimtouch/libraries/emulatorview/bin/AndroidManifest.xml : \
+/Users/momodalo/projects/vimtouch/vimtouch/libraries/emulatorview/AndroidManifest.xml \
---
+++ libraries/emulatorview/gen/R.java.d
@@ -0,0 +1,4 @@
+/Users/momodalo/projects/vimtouch/vimtouch/libraries/emulatorview/gen/jackpal/androidterm/emulatorview/R.java \
+ : /Users/momodalo/projects/vimtouch/vimtouch/libraries/emulatorview/res/drawable/atari_small.png \
+/Users/momodalo/projects/vimtouch/vimtouch/libraries/emulatorview/res/drawable-nodpi/atari_small_nodpi.png \
+/Users/momodalo/projects/vimtouch/vimtouch/libraries/emulatorview/bin/AndroidManifest.xml \
