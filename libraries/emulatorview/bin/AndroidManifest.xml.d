5f8dbdf11ca877048e9cd7589048f1a2641778d2
David Tao <momodalo@gmail.com>
2012-08-24 22:22:21 +0800

    remove generated files

--- libraries/emulatorview/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/momodalo/projects/vimtouch/vimtouch/libraries/emulatorview/bin/AndroidManifest.xml : \
-/Users/momodalo/projects/vimtouch/vimtouch/libraries/emulatorview/AndroidManifest.xml \
