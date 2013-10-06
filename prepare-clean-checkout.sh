be3d517560ae29c984594bb361c65ebc6315e614
Konstantin Vorobyev <kvorobyev@gmail.com>
2013-10-06 20:38:30 +0900

    Patch vim source code with integration-related changes

--- prepare-clean-checkout.sh
+++ prepare-clean-checkout.sh
@@ -16,6 +16,8 @@ mv vim73 vim
 rm vim/src/auto/config.h
 rm vim/src/feature.h
 rm vim-7.3.tar.bz2
+echo Applying integration.patch...
+patch -i ./integration.patch -p1
 wget http://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.14.tar.gz
 tar xzvf libiconv-1.14.tar.gz
 mv libiconv-1.14 libiconv
